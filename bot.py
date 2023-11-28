0b2fbfe2bbd2b30107f583050b8924f016412cf7
israel <israekbaruque992@gmail.com>
2023-11-28 19:43:33 -0300

    alt

--- bot.py
+++ bot.py
@@ -1,4 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from time import sleep
 
-browser = webdriver.Firefox()
-browser.get('http://selenium.dev/')
+browser = webdriver.Firefox(service=Service(r'C:\geckodriver\geckodriver.exe'))
+# browser = webdriver.Firefox()
+browser.get('http://google.com/')
+search = browser.find_element(By.XPATH, '//textarea[@id="APjFqb"]')
+search.clear()
+search.send_keys('Uma pesquisa comum')
+search.send_keys(Keys.ENTER)
+sleep(10)
+browser.quit()
